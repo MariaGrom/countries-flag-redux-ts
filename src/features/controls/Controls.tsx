b6e96f6f451cbec0a556d8d30e5cdbdbf15094ce
Maria Gromyko <mazvereva91@mail.ru>
2023-06-08 15:55:06 +0300

    fix: типизация контролеров

--- src/features/controls/Controls.tsx
+++ src/features/controls/Controls.tsx
@@ -3,9 +3,9 @@ import styled from 'styled-components';
 import { Search } from './Search';
 import { CustomSelect } from './CustomSelect';
 import { useRegion } from './use-region';
+import { Region } from 'types';
 
-
-const optionsMap = {
+const optionsMap: Record <Region, {value: Region, label: Region}> = {
   'Africa': { value: 'Africa', label: 'Africa' },
   'America': { value: 'America', label: 'America' },
   'Asia': { value: 'Asia', label: 'Asia' },
@@ -37,7 +37,7 @@ export const Controls = () => {
         placeholder="Filter by Region"
         isClearable
         isSearchable={false}
-        value={optionsMap[region] || ''}
+        value={region ? optionsMap[region] : ''}
         onChange={handleSelect}
       />
     </Wrapper>
